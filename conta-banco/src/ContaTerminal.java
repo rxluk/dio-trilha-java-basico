1fb0e362782a014dd4330b09cfe1202ebb684b38
rxluk <lucasg.exe@gmail.com>
2023-08-26 15:30:40 -0300

    Initial commit: : Estrutura básica e entrada de dados

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        int numero;
+        String agencia;
+        String cliente;
+        double saldo;
+
+        System.out.println("Por favor, digite o número da Conta !");
+        numero = scanner.nextInt();
+        System.out.println("Por favor, digite o número da Agência !");
+        agencia = scanner.next();
+        System.out.println("Por favor, digite o nome do cliente !");
+        cliente = scanner.next();
+        System.out.println("Por favor, digite o saldo !");
+        saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + cliente + ", obrigado por criar uma conta em nosso banco, sua agência é " 
+            + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");
+
+        scanner.close();
     }
 }
